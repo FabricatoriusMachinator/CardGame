74675d28fea8e7b191119a297faac420fda4f5d7
FabricatoriusMachinator <mafl96@hotmail.com>
2020-11-02 03:03:39 +0100

    Some adjustments and added some stuff

---
+++ CardGame/GameManagr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class GameManagr
+    {
+        private static readonly GameManagr instance = new GameManagr();
+        public static Deck playDeck = new Deck();
+        
+        private void startGame()
+        {
+            playDeck.createDeck();
+            playDeck.Shuffle();
+        }
+
+        public static GameManagr getInstance()
+        {
+            return instance;
+        }
+
+        public Deck getDeck()
+        {
+            return playDeck;
+        }
+
+    }
+}
--- CardGame/Player.cs
+++ CardGame/Player.cs
@@ -10,17 +10,35 @@ namespace CardGame
     {
         string player;
         Card[] hand = new Card[3];
+        GameManagr gameMGMT = GameManagr.getInstance();
+        int cardCount = 0;
+        int cardCap = 4;
+        int index = 0; //TODO: Create selection algorithm
 
-        public void drawCard()
+        private void drawCard()
         {
+            int cntr = index;
+            index = cardCap - 1;
             
+
+            if (cardCount == 4 || cardCount == 5)
+            {
+                hand[index] = null;
+            }
+
+            while (cardCount < 4)
+            {
+
+                hand[cntr] = GameManagr.playDeck.deck[GameManagr.playDeck.topOfDeck];
+                cntr += 1;
+                GameManagr.playDeck.topOfDeck -= 1;
+            }
         }
 
-        public Deck[] getDeck()
+        public string printName()
         {
-            return GameManager.playDeck
+            return player;
         }
-
 
     }
 
